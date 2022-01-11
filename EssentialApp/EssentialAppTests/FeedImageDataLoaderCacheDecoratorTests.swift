dab234a0e77f8c4bda9a1e3df02eae42a809bf3b
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2022-01-11 12:13:23 +0530

    'FeedImageDataloaderCacheDecorator.loadImageData' delivers error on loader failure.

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -42,16 +42,23 @@ class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
 
         XCTAssertEqual(loader.cancelledURLs, [url], "Expected to cancel URL from loader")
     }
-    
+
     func test_loadImageData_deliversDataOnLoaderSuccess() {
         let imageData = anyData()
         let (sut, loader) = makeSUT()
-        
+
         expect(sut, toCompleteWith: .success(imageData), when: {
             loader.complete(with: imageData)
         })
     }
-    
+
+    func test_loadImageData_deliversErrorOnLoaderFailure() {
+        let (sut, loader) = makeSUT()
+
+        expect(sut, toCompleteWith: .failure(anyNSError()), when: {
+            loader.complete(with: anyNSError())
+        })
+    }
 
     // MARK: - Helpers
 
@@ -62,24 +69,23 @@ class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, loader)
     }
-    
-    private func expect(_ sut: FeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action:()->Void, file: StaticString = #file, line: UInt = #line) {
-        
+
+    private func expect(_ sut: FeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
         _ = sut.loadImageData(from: anyURL()) { recievedResult in
-            switch(recievedResult, expectedResult) {
-                case let (.success(recievedFeed), .success(expectedFeed)):
-                    XCTAssertEqual(recievedFeed, expectedFeed, file: file, line: line)
-                case (.failure, .failure):
-                    break
-                    
-                default:
-                    XCTFail("Expected \(expectedResult), got \(recievedResult) instead", file: file, line: line)
+            switch (recievedResult, expectedResult) {
+            case let (.success(recievedFeed), .success(expectedFeed)):
+                XCTAssertEqual(recievedFeed, expectedFeed, file: file, line: line)
+            case (.failure, .failure):
+                break
+
+            default:
+                XCTFail("Expected \(expectedResult), got \(recievedResult) instead", file: file, line: line)
             }
             exp.fulfill()
         }
         action()
-        
+
         wait(for: [exp], timeout: 1.0)
     }
 
@@ -103,9 +109,13 @@ class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
                 self?.cancelledURLs.append(url)
             }
         }
-        
+
         func complete(with data: Data, at index: Int = 0) {
             messages[index].completion(.success(data))
+        }
+
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
         }
     }
 }
