bfc32b342f3e5f438d8e845b569329ae107a2b38
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2022-01-12 17:48:04 +0530

    Test Failed - UI displays remote feed when customer has connectivity

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -13,7 +13,27 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
 
         app.launch()
 
-        XCTAssertEqual(app.cells.count, 22)
-        XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(feedCells.count, 22)
+
+        let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstImage.exists)
     }
+
+    /* func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
+
+         let onlineApp = XCUIApplication()
+         onlineApp.launch()
+
+         let offlineApp = XCUIApplication()
+         offlineApp.launchArguments = ["-connectivity", "offline"]
+         offlineApp.launch()
+
+         let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
+         XCTAssertEqual(cachedFeedCells.count, 22)
+
+         let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
+         XCTAssertTrue(firstCachedImage.exists)
+
+     } */
 }
