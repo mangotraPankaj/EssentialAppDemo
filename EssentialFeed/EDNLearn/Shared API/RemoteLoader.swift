1cc31c78c540244642b60dc3c4991e0f11336251
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2022-02-14 12:01:02 +0530

    Duplicate REmoteFeedLoader as RemoteLoader

---
+++ EssentialFeed/EDNLearn/Shared API/RemoteLoader.swift
@@ -0,0 +1,46 @@
+//
+//  RemoteLoader.swift
+//  EDNLearn
+//
+//  Created by Pankaj Mangotra on 14/02/22.
+//
+
+import Foundation
+
+public final class RemoteLoader: FeedLoader {
+    private let client: HTTPClient
+    private let url: URL
+
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+
+    public typealias Result = FeedLoader.Result
+    //        case success([Feeditem])
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case let .success((data, response)):
+                completion(RemoteLoader.map(data, from: response))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemMapper.map(data, from: response)
+            return .success(items)
+        } catch {
+            return .failure(Error.invalidData)
+        }
+    }
+}
