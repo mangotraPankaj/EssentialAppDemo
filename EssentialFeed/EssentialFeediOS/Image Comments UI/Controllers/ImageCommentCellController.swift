58dce7ffe72c1a45fe67a3ff5abaece462544245
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2022-03-11 13:45:39 +0530

    Implement Image comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageCommentCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Pankaj Mangotra on 11/03/22.
+//
+
+import EDNLearnMac
+import UIKit
+
+public class ImageCommentCellController: CellController {
+    private let model: ImageCommentViewModel
+
+    public init(model: ImageCommentViewModel) {
+        self.model = model
+    }
+
+    public func view(in tableView: UITableView) -> UITableViewCell {
+        let cell: ImageCommentCell = tableView.dequeueReusableCell()
+        cell.messageLabel.text = model.message
+        cell.dateLabel.text = model.date
+        cell.usernameLabel.text = model.username
+        return cell
+    }
+
+    public func preload() {}
+
+    public func cancelLoad() {}
+}
---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Pankaj Mangotra on 11/03/22.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet public private(set) var usernameLabel: UILabel!
+    @IBOutlet public private(set) var dateLabel: UILabel!
+    @IBOutlet public private(set) var messageLabel: UILabel!
+}
