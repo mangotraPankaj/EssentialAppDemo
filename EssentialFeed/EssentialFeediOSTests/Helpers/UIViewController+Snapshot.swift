1b9e3f1c67ad3632bcc590542174a0d396cd3149
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2022-03-24 13:22:14 +0530

    Changed the snapshot configuration and committing the new snapshots

--- EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
@@ -10,61 +10,53 @@ import UIKit
 
 extension UIViewController {
     func snapshot(for configuration: SnapshotConfiguration) -> UIImage {
-        // return SnapshotWindow(root: self, userStyle: .dark).snapshot()
         return SnapshotWindow(configuration: configuration, root: self).snapshot()
     }
 }
 
 struct SnapshotConfiguration {
-//    let size: CGSize
-//    let safeAreaInsets: UIEdgeInsets
-//    let layoutMargins: UIEdgeInsets
+    let size: CGSize
+    let safeAreaInsets: UIEdgeInsets
+    let layoutMargins: UIEdgeInsets
     let traitCollection: UITraitCollection
 
     static func iPhone13(style: UIUserInterfaceStyle) -> SnapshotConfiguration {
-        return SnapshotConfiguration(traitCollection: UITraitCollection(traitsFrom: [.init(userInterfaceStyle: style)]))
-//        return SnapshotConfiguration(
-//            size: CGSize(width: 390, height: 844),
-//            safeAreaInsets: UIEdgeInsets(top: 47, left: 0, bottom: 34, right: 0),
-//            layoutMargins: UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16),
-//            traitCollection: UITraitCollection(traitsFrom: [
-//                .init(forceTouchCapability: .unavailable),
-//                .init(layoutDirection: .leftToRight),
-//                .init(preferredContentSizeCategory: .large),
-//                .init(userInterfaceIdiom: .phone),
-//                .init(horizontalSizeClass: .compact),
-//                .init(verticalSizeClass: .regular),
-//                .init(displayScale: 3),
-//                .init(displayGamut: .P3),
-//                .init(userInterfaceStyle: style),
-//            ])
-//        )
+        return SnapshotConfiguration(
+            size: CGSize(width: 390, height: 844),
+            safeAreaInsets: UIEdgeInsets(top: 47, left: 0, bottom: 34, right: 0),
+            layoutMargins: UIEdgeInsets(top: 55, left: 8, bottom: 42, right: 8),
+            traitCollection: UITraitCollection(traitsFrom: [
+                .init(forceTouchCapability: .unavailable),
+                .init(layoutDirection: .leftToRight),
+                .init(preferredContentSizeCategory: contentSize),
+                .init(userInterfaceIdiom: .phone),
+                .init(horizontalSizeClass: .compact),
+                .init(verticalSizeClass: .regular),
+                .init(displayScale: 3),
+                .init(accessibilityContrast: .normal),
+                .init(displayGamut: .P3),
+                .init(userInterfaceStyle: style),
+            ])
+        )
     }
 }
 
 private final class SnapshotWindow: UIWindow {
     private var configuration: SnapshotConfiguration = .iPhone13(style: .light)
 
-//    convenience init(root: UIViewController, userStyle: UIUserInterfaceStyle) {
-//        self.init(frame: CGRect(origin: .zero, size: root.view.bounds.size))
-//        rootViewController = root
-//        overrideUserInterfaceStyle = userStyle
-//        isHidden = false
-//    }
-
     convenience init(configuration: SnapshotConfiguration, root: UIViewController) {
-        self.init(frame: CGRect(origin: .zero, size: root.view.bounds.size))
+        self.init(frame: CGRect(origin: .zero, size: configuration.size))
         self.configuration = configuration
-        // layoutMargins = configuration.layoutMargins
+        layoutMargins = configuration.layoutMargins
         rootViewController = root
         isHidden = false
-        // root.view.layoutMargins = configuration.layoutMargins
+        root.view.layoutMargins = configuration.layoutMargins
     }
 
-//    override var safeAreaInsets: UIEdgeInsets {
-//        return configuration.safeAreaInsets
-//    }
-//
+    override var safeAreaInsets: UIEdgeInsets {
+        return configuration.safeAreaInsets
+    }
+
     override var traitCollection: UITraitCollection {
         return UITraitCollection(traitsFrom: [super.traitCollection, configuration.traitCollection])
     }
